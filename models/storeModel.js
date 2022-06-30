6955f21ac4d3c5a22f827c15bd9fb2553a42378e
roberio <roberio-almeida@hotmail.com>
2022-06-29 23:39:10 -0300

    feat: fetches product in database and returns via model

--- models/storeModel.js
+++ models/storeModel.js
@@ -1,11 +1,22 @@
 const { connection } = require('../helpers');
 
 const getAll = async () => {
-  const [products] = await connection.execute(`
-    SELECT *
+  const [products] = await connection.execute(
+    `SELECT *
       FROM StoreManager.products
-        ORDER BY id ASC`);
+        ORDER BY id ASC`
+  );
   return products;
 };
 
-module.exports = { getAll };
+const getById = async (id) => {
+  const [product] = await connection.execute(
+    `SELECT *
+      FROM StoreManager.products
+        WHERE id = ?`,
+    [id],
+  );
+  return product;
+};
+
+module.exports = { getAll, getById };
--- services/storeService.js
+++ services/storeService.js
@@ -7,7 +7,9 @@ const getAll = async () => {
 
 const getById = async (id) => {
   const product = await storeModel.getById(id);
-  return product;
+  return product[0];
+  //Expected: {"id": 1, "name": "Martelo de Thor"}
+  //Received: [{"id": 1, "name": "Martelo de Thor"}]
 };
 
 module.exports = { getAll, getById };
