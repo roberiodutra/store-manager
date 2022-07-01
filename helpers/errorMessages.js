10fc5d950f559af423d41042d2543fc6ec116e5c
roberio <roberio-almeida@hotmail.com>
2022-07-01 01:18:23 -0300

    feat: create salesModel and return new sale ID

--- helpers/errorMessages.js
+++ helpers/errorMessages.js
@@ -3,9 +3,9 @@ const errorMessages = {
   NOT_FOUND: { message: 'Product not found' },
   NAME_REQUIRED: { message: '"name" is required' },
   INVALID_NAME: { message: '"name" length must be at least 5 characters long' },
-  ID_REQUIRED: { "message": '\"productId\" is required' },
-  QUANTITY_REQUIRED: { "message": '"quantity" is required' },
-  INVALID_QUANTITY: { "message": '"quantity" must be greater than or equal to 1' },
+  ID_REQUIRED: { message: '"productId" is required' },
+  QUANTITY_REQUIRED: { message: '"quantity" is required' },
+  INVALID_QUANTITY: { message: '"quantity" must be greater than or equal to 1' },
 };
 
 module.exports = errorMessages;
