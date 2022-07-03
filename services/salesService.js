f4ca7fd8c7ffe92339e64719c3cacda335361e65
roberio <roberio-almeida@hotmail.com>
2022-07-03 16:34:45 -0300

    refactor: change some variable names

--- services/salesService.js
+++ services/salesService.js
@@ -5,10 +5,10 @@ const productsModel = require('../models/productsModel');
 const createSale = async (sales, res) => {
   const products = await productsModel.getAll();
   const productsIds = products.map(({ id }) => id);
-  const salesId = sales.map((s) => s.productId)
-    .every((saleId) => productsIds.includes(saleId));
+  const salesIds = sales.map((s) => s.productId)
+    .every((each) => productsIds.includes(each));
 
-  if (productId(sales, salesId, res) || quantity(sales, res)) return;
+  if (productId(sales, salesIds, res) || quantity(sales, res)) return;
 
   const saleId = await salesModel.createSale();
   await Promise.all(
