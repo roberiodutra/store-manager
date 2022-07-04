b0dd9b05c44743ab3fa8e0fb0f2127847791c82e
roberio <roberio-almeida@hotmail.com>
2022-07-03 23:25:41 -0300

    feat: create new get routes for sales

--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -4,6 +4,8 @@ const salesController = require('../controllers/salesController');
 
 const salesRouter = express.Router();
 
+salesRouter.get('/', salesController.getAll);
+salesRouter.get('/:id', salesController.getById);
 salesRouter.post('/', salesController.createSale);
 
 module.exports = salesRouter;
