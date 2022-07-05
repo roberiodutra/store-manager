e0d96d96e5f7e61632dbcb0de26caebfe05a0442
roberio <roberio-almeida@hotmail.com>
2022-07-05 17:00:58 -0300

    feat: create update route

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -7,5 +7,6 @@ const productsRouter = express.Router();
 productsRouter.get('/', productsController.getAll);
 productsRouter.get('/:id', productsController.getById);
 productsRouter.post('/', productsController.add);
+productRouter.put('/:id', productController.update);
 
 module.exports = productsRouter;
